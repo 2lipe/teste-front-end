c6a7204ad2c0273927f2221aef0b9dfea17413e2
Felipe <felipe.svfx@gmail.com>
2020-07-10 02:03:09 -0300

    finalizado implementacao basica

--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
-(function() {
-  const usersProfiles = document.getElementById('profile');
-  const url = 'https://reqres.in/api/users?page=1';
+/* eslint-disable no-undef */
+const usersProfiles = document.getElementById('profile');
+const url = 'https://reqres.in/api/users';
 
-  async function getUsers() {
-    const usersResponse = await fetch(url);
-    const profile = usersResponse.json();
+const getUsers = async () => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const profile = await res.json();
     return profile;
+  } catch (error) {
+    return error;
   }
+};
 
-  function showUsers() {
-    
-  }
+function showUsers(users) {
+  let output = '';
 
-  window.addEventListener('load', (e) => {
-    getUsers().then(res => console.log(res));
+  users.forEach((user) => {
+    output += `<div class="column">
+    <div class="card">
+    <img class="ico" src="./assets/icon-edit.svg" />
+    <img class="avatar" src="${user.avatar}" />
+    <h1>${user.first_name} ${user.last_name}</h1>
+    <p>${user.email}</p>
+    </div>
+    </div>`;
   });
-})();
+
+  usersProfiles.innerHTML = output;
+}
+
+window.addEventListener('load', () => {
+  getUsers().then((res) => showUsers(res.data));
+});

94d90748290f2f3b26787c4ce2f10d21ca3037e7
Felipe <felipe.svfx@gmail.com>
2020-07-10 17:18:42 -0300

    refact

--- src/send.js
+++ src/send.js
@@ -2,26 +2,30 @@
 const form = document.getElementById('request-form');
 const url = 'https://reqres.in/api/login';
 
-function submitForm() {
+const postForm = async () => {
   const formData = new FormData(form);
   const searchParams = new URLSearchParams();
 
+  // eslint-disable-next-line no-restricted-syntax
   for (const pair of formData) {
     searchParams.append(pair[0], pair[1]);
-  };
-
-  fetch(url, {
-    method: 'post',
-    body: searchParams,
-  }).then((data) => {
-    return data.json();
-  }).then((res) => {
-    return res;
-  }).catch((error) => {
+  }
+  try {
+    const res = await fetch(url, {
+      method: 'post',
+      body: searchParams,
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
     return error;
+  }
+};
+
+function submitForm() {
+  form.addEventListener('submit', () => {
+    postForm();
   });
 }
 
-form.addEventListener('submit', () => {
-  submitForm();
-});
+submitForm();

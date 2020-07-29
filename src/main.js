ce1590d0b1b5a98ccebc315ce1bc685566f6cfc4
Felipe <felipe.svfx@gmail.com>
2020-07-29 11:19:46 -0300

    corrigindo templates

--- src/main.js
+++ src/main.js
@@ -1,45 +1,10 @@
-/* eslint-disable no-undef */
-const usersProfiles = document.getElementById('profile');
-const footerNumber = document.getElementById('footer');
-const url = 'https://reqres.in/api/users';
+/** Libs para uso de funções avançadas do JS em navegadores antigos */
+import 'regenerator-runtime/runtime';
+import 'core-js';
 
-const getUsers = async () => {
-  try {
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    const profile = await res.json();
-    return profile;
-  } catch (error) {
-    return error;
-  }
-};
+/** Styles */
+import './styles/sass/main.scss';
 
-function showUsers(users) {
-  let output = '';
-
-  users.forEach((user) => {
-    output += `<div class="column">
-    <div class="card">
-    <img class="ico" src="./assets/icon-edit.svg" />
-    <img class="avatar" src="${user.avatar}" />
-    <h1>${user.first_name} ${user.last_name}</h1>
-    <p>${user.email}</p>
-    </div>
-    </div>`;
-  });
-
-  usersProfiles.innerHTML = output;
-}
-
-function showNumber(numbers) {
-  footerNumber.innerHTML = `<span>Mostrando ${numbers.per_page} de ${numbers.total}</span>`;
-}
-
-window.addEventListener('load', () => {
-  getUsers().then((res) => {
-    showUsers(res.data);
-    showNumber(res);
-  });
-});
+/** Scripts */
+import './js/main';
+import './js/authentication';
